bdc58f78d16e6af08b34f5f5b9d314d2fcad631c
akpatel21 <43002673+akpatel21@users.noreply.github.com>
2018-10-25 08:37:59 -0400

    Add files via upload

---
+++ src/StringManipulator.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Akul Patel
+ * String Manipulator
+ *
+ */
+import java.util.Scanner;
+
+public class StringManipulator 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Please enter a string: ");
+		String string = keyboard.nextLine();
+		int length = string.length();
+		System.out.println("\nYour string is " + length +)
+		
+	}
+
+}
